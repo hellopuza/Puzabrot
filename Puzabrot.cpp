03033f373c7b3b6e2f0cd140ce77d17281e61229
Artem Puzankov <71190274+hellopuza@users.noreply.github.com>
2021-06-30 19:21:48 +0300

    Window resizing added

--- Puzabrot.cpp
+++ Puzabrot.cpp
@@ -12,23 +12,92 @@
 
 //------------------------------------------------------------------------------
 
-Puzabrot::Puzabrot ()
+Puzabrot::Puzabrot () :
+    winsizes_ ({DEFAULT_WIDTH, DEFAULT_HEIGHT})
 {
+    pointmap_ = new sf::VertexArray(sf::Points, winsizes_.x * winsizes_.y);
+    window_   = new sf::RenderWindow(sf::VideoMode(winsizes_.x, winsizes_.y), title_string);
+    
+    borders_.Im_up   =  1.3;
+    borders_.Im_down = -1.3;
 
+    borders_.Re_left  = -(borders_.Im_up - borders_.Im_down) * winsizes_.x/winsizes_.y / 5 *3;
+    borders_.Re_right =  (borders_.Im_up - borders_.Im_down) * winsizes_.x/winsizes_.y / 5 *2;
 }
 
 //------------------------------------------------------------------------------
 
 Puzabrot::~Puzabrot ()
 {
-
+    if (window_->isOpen()) window_->close();
+    delete pointmap_;
+    delete window_;
 }
 
 //------------------------------------------------------------------------------
 
 void Puzabrot::run ()
 {
+    window_->setVerticalSyncEnabled(true);
 
+    while (window_->isOpen())
+    {
+        sf::Event event;
+        while (window_->pollEvent(event))
+        {
+            if ( ( event.type == sf::Event::Closed) || 
+                 ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) )
+            {
+                window_->close();
+                return;
+            }
+
+            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::F11))
+            {
+                toggleFullScreen();
+            }
+
+            if (event.type == sf::Event::Resized)
+            {
+                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                window_->setView(sf::View(visibleArea));
+                updateWinSizes(window_->getSize().x, window_->getSize().y);
+            }
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+void Puzabrot::updateWinSizes (size_t width, size_t height)
+{
+    winsizes_.x = width;
+    winsizes_.y = height;
+
+    delete pointmap_;
+    pointmap_ = new sf::VertexArray(sf::Points, winsizes_.x * winsizes_.y);
+
+    borders_.Re_right = borders_.Re_left + (borders_.Im_up - borders_.Im_down) * winsizes_.x/winsizes_.y;
+    /*DrawMandelbrot();*/
+}
+
+//------------------------------------------------------------------------------
+
+void Puzabrot::toggleFullScreen ()
+{
+    if (window_->isOpen()) window_->close();
+    delete window_;
+
+    if ((winsizes_.x == sf::VideoMode::getDesktopMode().width) && (winsizes_.y == sf::VideoMode::getDesktopMode().height))
+    {
+        window_ = new sf::RenderWindow(sf::VideoMode(DEFAULT_WIDTH, DEFAULT_HEIGHT), title_string);
+        updateWinSizes(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+    else
+    {
+        window_ = new sf::RenderWindow(sf::VideoMode::getDesktopMode(), title_string, sf::Style::Fullscreen);
+        updateWinSizes(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
+    }
 }
 
 //------------------------------------------------------------------------------
--- Puzabrot.h
+++ Puzabrot.h
@@ -12,7 +12,6 @@
 
 #include <SFML/Graphics.hpp>
 #include <assert.h>
-#include <string.h>
 #include <omp.h>
 
 #include <emmintrin.h>
@@ -24,6 +23,24 @@ const int DEFAULT_WIDTH  = 640;
 const int DEFAULT_HEIGHT = 480;
 
 const sf::String title_string = "Puzabrot";
+
+
+struct Screen
+{
+    int    x1   = -1;
+    int    x2   = -1;
+    int    y1   = -1;
+    int    y2   = -1;
+    double zoom =  0;
+};
+
+struct ComplexFrame
+{
+    double Re_left  = 0;
+    double Re_right = 0;
+    double Im_up    = 0;
+    double Im_down  = 0;
+};
 
 //------------------------------------------------------------------------------
 
@@ -38,6 +55,17 @@ public:
 
 private:
 
+    sf::RenderWindow* window_ = nullptr;
+    sf::VertexArray*  pointmap_ = nullptr;
+    ComplexFrame      borders_;
+    sf::Vector2i      winsizes_;
+
+    size_t delta_zoom_ = 3000;
+    double lim_        = 100;
+    size_t itrn_max_   = 3000;
+
+    void updateWinSizes (size_t width, size_t height);
+    void toggleFullScreen ();
 };
 
 //------------------------------------------------------------------------------
